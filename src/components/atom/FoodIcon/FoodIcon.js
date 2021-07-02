613042a7b82e6bb7910dfe5d5ae85f56303548a1
Huy-No1 <71492647+Huy-No1@users.noreply.github.com>
2021-07-02 17:30:04 +0700

    4

--- src/components/atom/FoodIcon/FoodIcon.js
+++ src/components/atom/FoodIcon/FoodIcon.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react'
 import styles from './FoodIcon.module.scss'
-
+import { motion } from "framer-motion";
 function FoodIcon(props) {
     const {type, active} =props;
 //onClick={handleActive}
     return (
         <div className={styles.mainComponent} >
-            <div className={styles.img}>
+            {/* <div className={styles.img}>
                 <img src={`image/svg/foodIcon/${type}.svg`} alt="" />
-            </div>
-            <div className={active ? styles.img2: styles.img3}>
-                <img src={`image/svg/foodIcon/${type}-active.svg`} alt="" />
-            </div>
+            </div> */}
+            <motion.div className={styles.img} >
+            {
+                active ? <motion.img 
+                    initial={{opacity: 0.1}}
+                    animate={{opacity: 1}}
+                    transition={{duration: 0.4}}
+                    
+                    src={`image/svg/foodIcon/${type}-active.svg`} alt="" /> :
+                <img 
+                    // initial={{opacity: 0.1}}
+                    // animate={{opacity: 1}}
+                    src={`image/svg/foodIcon/${type}.svg`} alt="" />
+            
+            }
+                    {/* <img src={`image/svg/foodIcon/${type}-active.svg`} alt="" /> */}
+            </motion.div>
     
             <p className={styles.name}>{type}</p>
         </div>
