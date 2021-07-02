502e8ff9aafc4239e6f92a7fcfc3a6be95beafc4
Huy-No1 <71492647+Huy-No1@users.noreply.github.com>
2021-07-02 17:36:14 +0700

    4

--- src/components/atom/FoodIcon/FoodIcon.js
+++ src/components/atom/FoodIcon/FoodIcon.js
@@ -9,22 +9,22 @@ function FoodIcon(props) {
             {/* <div className={styles.img}>
                 <img src={`image/svg/foodIcon/${type}.svg`} alt="" />
             </div> */}
-            <motion.div className={styles.img} >
             {
                 active ? <motion.img 
                     initial={{opacity: 0.1}}
                     animate={{opacity: 1}}
                     transition={{duration: 0.4}}
-                    
+                    className={styles.img}
                     src={`image/svg/foodIcon/${type}-active.svg`} alt="" /> :
                 <img 
                     // initial={{opacity: 0.1}}
                     // animate={{opacity: 1}}
+                    className={styles.img}
                     src={`image/svg/foodIcon/${type}.svg`} alt="" />
             
             }
                     {/* <img src={`image/svg/foodIcon/${type}-active.svg`} alt="" /> */}
-            </motion.div>
+    
     
             <p className={styles.name}>{type}</p>
         </div>
